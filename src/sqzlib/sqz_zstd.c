678a3dd962f1e7c2e2b3c31797f9f8fad3704623
Julian Regalado <jregalado@bicu.dev>
2021-08-24 10:45:58 +0200

    Forgot to add sqz_zstd.c

---
+++ src/sqzlib/sqz_zstd.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <zstd.h>
+#include "sqz_data.h"
+
+int64_t sqz_zstdcompress(sqzblock_t *blk, int level)
+{
+    int64_t ret = 0;
+
+    ret = ZSTD_compress(blk->cmpbuff, blk->cmpsize,
+                        blk->blkbuff, blk->blkpos, level);
+    if (ZSTD_isError(ret)) return -1;
+
+    return ret;
+}
+
+
+uint64_t sqz_zstddecompress(sqzblock_t *blk)
+{
+    int64_t ret = 0;
+
+    ret = ZSTD_decompress(blk->blkbuff, blk->blksize,
+                          blk->cmpbuff, blk->cmpsize);
+    if (ZSTD_isError(ret)) return 0;
+    return (uint64_t)ret;
+}
